1e0ce62b8d167e98e5d1ad8a2b0163e68308dfa1
AlimuratNur <alimratnurgajn@gmail.com>
2025-09-25 19:49:29 +0500

    windowsize rework, added getwindowsize helper method for windowsetsize

--- MainScene.cs
+++ MainScene.cs
@@ -12,6 +12,7 @@ public partial class MainScene : Node2D
 {
     
     private GridOfMines _grid;
+
     public override void _Ready()
     {
         DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
@@ -27,25 +28,29 @@ public partial class MainScene : Node2D
         restartButton.Position = new Vector2(5*32-restartButton.Size.X/2 + 15, 0);
         restartButton.ButtonDown += () => RestartGame();
 
-        
+        var restartButtonSizeY= (int)restartButton.Size.Y;
         switch (difficutlt)
         {
+            
             case Difficults.Easy:
                 _grid.Init(10, 15, 40);
-                DisplayServer.WindowSetSize(new Vector2I(10*32 + 15, 20*32 -20));
+                DisplayServer.WindowSetSize(GetWindowSize(10,15,restartButtonSizeY));
                 break;
             case Difficults.Medium:
                 _grid.Init(15, 20, 80);
-                DisplayServer.WindowSetSize(new Vector2I(10 * 32, 15 * 32));
+                DisplayServer.WindowSetSize(GetWindowSize(15 ,20, restartButtonSizeY));
                 break;
             case Difficults.Hard:
                 _grid.Init(20, 30, 120);
-                DisplayServer.WindowSetSize(new Vector2I(20 * 32, 30 * 32 + (int)restartButton.Size.X));
+                DisplayServer.WindowSetSize(GetWindowSize(20, 30,restartButtonSizeY));
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(difficutlt), difficutlt, null);
         }
     }
+
+    private Vector2I GetWindowSize(int width, int height, int buttonsize) 
+        => new Vector2I(width * 32 + 15, height * 32 + buttonsize); 
 
     private void RestartGame()
     {
