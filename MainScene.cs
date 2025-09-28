515edd6b7b4e4b14634b7632d63fd34938f09932
AlimuratNur <alimratnurgajn@gmail.com>
2025-09-28 23:34:22 +0500

    restart button's logic changes

--- MainScene.cs
+++ MainScene.cs
@@ -2,8 +2,6 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-
-
 public enum Difficults{
     Easy,
     Medium,
@@ -12,8 +10,9 @@ public enum Difficults{
 
 public partial class MainScene : Node2D
 {
-    private bool isfirstgame = true;
+    public bool isfirstgame { get; private set; } = true;
     private GridOfMines _grid;
+
 
     private static Dictionary<Difficults, Tuple<int,int,int>> _DifficultsSettings = new Dictionary<Difficults, Tuple<int, int, int>>
     {
@@ -22,63 +21,43 @@ public partial class MainScene : Node2D
         [Difficults.Expert] = Tuple.Create(15,20,80)
     };
 
-    public override void _Ready()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
-        _grid = GetNode<GridOfMines>("GridOfMines");
-        
-    }
+    
     
     public void Init(Difficults difficult)
     {
+        if (isfirstgame)
+            isfirstgame = false;
+        else RestartGame();
         
-        var restartButton = GetNode<Button>("RestartButton");
-        restartButton.ButtonDown += () => RestartGame();
-        var restartButtonSizeY= (int)restartButton.Size.Y;
-        _grid.GameIsWon += () => 
-            GetNode<Label>("ShowFlagsCount").Text = "You won";
-
         var width = _DifficultsSettings[difficult].Item1;
         var height = _DifficultsSettings[difficult].Item2;
         var mineCount = _DifficultsSettings[difficult].Item3;
         
-        restartButton.Position = new Vector2(width*16-restartButton.Size.X/2 , 0);
-
+        var restartButton = GetNode<Button>("RestartButton");
+        restartButton.ButtonDown += () => RestartGame();
+        var restartButtonSizeY = (int)restartButton.Size.Y;
+        restartButton.Position = 
+                new Vector2(width*16-restartButton.Size.X/2 , 0);
+        
+        _grid = GetNode<GridOfMines>("GridOfMines");
         _grid.Init(width, height, mineCount);
-        DisplayServer.WindowSetSize(GetWindowSize((int)_grid.Width, (int)_grid.Height, restartButtonSizeY));
+        _grid.GameIsWon += () => 
+            GetNode<Label>("ShowFlagsCount").Text = "You won";
 
-        /*switch (difficult)
-        {
-            
-            case Difficults.Easy:
-                _grid.Init(10, 15, 40);
-                DisplayServer.WindowSetSize(GetWindowSize(10,15,restartButtonSizeY));
-                break;
-            case Difficults.Medium:
-                _grid.Init(15, 20, 80);
-                DisplayServer.WindowSetSize(GetWindowSize(15 ,20, restartButtonSizeY));
-                break;
-            case Difficults.Expert:
-                _grid.Init(20, 25, 100);
-                DisplayServer.WindowSetSize(GetWindowSize(20, 25,restartButtonSizeY));
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(difficult), difficult, null);
-        }*/
+        DisplayServer
+            .WindowSetMode(DisplayServer.WindowMode.Windowed);
+        DisplayServer
+            .WindowSetSize(GetWindowSize((int)_grid.Width, (int)_grid.Height, restartButtonSizeY));
     }
+
+    public void RestartGame()
+    {
+        _grid.GetTree().ReloadCurrentScene();
+    }
+
+  
 
     private Vector2I GetWindowSize(int width, int height, int buttonSize) 
         => new ((int)(width * 32 * 0.8), (int)(height * 32 * 0.8) + buttonSize / 2 + 9); 
 
-    private void RestartGame()
-    {
-        GetTree().ReloadCurrentScene();
-        
-    }
-
-    private void RestartGame(int a)
-    {
-        GetTree().ReloadCurrentScene();
-
-    }
 }
--- MainWindowScene.cs
+++ MainWindowScene.cs
@@ -5,6 +5,7 @@ public partial class MainWindowScene : Node2D
     private int _Width;
     private int _Height;
     private int _MineCount;
+    private int _CurrentDifficult; 
     private MainScene Playground;
 
     public override void _Ready()
@@ -13,11 +14,15 @@ public partial class MainWindowScene : Node2D
         Playground.Hide();
         var choseDiffWindow = GetNode<GetChosedDifficult>("GetChosedDifficult");
         choseDiffWindow.SetDiff += x => _DiffcultsButtonDown(x);
-        
+        var window = GetNode<GetChosedDifficult>("GetChosedDifficult");
+        window.CloseRequested += () => this.Hide();
+
     }
 
     private void _DiffcultsButtonDown(int diff)
     {
+        _CurrentDifficult = diff;
+        
         Playground.Init((Difficults)diff);
         Playground.Show();
         GetNode<GetChosedDifficult>("GetChosedDifficult").Hide();
@@ -28,6 +33,11 @@ public partial class MainWindowScene : Node2D
         if (Input.IsKeyPressed(Key.H))
         {
             GetNode<GetChosedDifficult>("GetChosedDifficult").Show();
+        }
+
+        if (Input.IsActionPressed("ui_text_scroll_up"))
+        {
+            DisplayServer.ScreenGetScale(2);
         }
     }
 }
